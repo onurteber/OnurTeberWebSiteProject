52f656da62ef7fd7bef21632e22c4bf51b31a3fd
onurteber <onurteber@users.noreply.github.com>
2019-02-20 16:42:04 +0300

    second commit

---
+++ OnurTeberWebSite/OnurTeberWebSite/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnurTeberWebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        // HomePage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
